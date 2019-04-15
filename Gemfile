d6b532c6e7a33fb1143f2cf59c36301b4e1b5b8c
tomon9086 <tomo.mouse.9086@gmail.com>
2019-04-15 10:45:59 +0900

    markdownが使える かんたんですごい

--- Gemfile
+++ Gemfile
@@ -60,3 +60,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# markdown
+gem 'redcarpet', '~> 2.3.0'
+# syntax-highlight in markdown
+gem 'coderay'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  require "redcarpet"
+  require "coderay"
+  # -> markdown: https://qiita.com/hkengo/items/978ea1874cf7e07cdbfc
+
+  class HTMLwithCoderay < Redcarpet::Render::HTML
+    def block_code(code, language)
+      language = language.split(':')[0]
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown(text)
+    html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
+    options = {
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      tables: true,
+      hard_wrap: true,
+      xhtml: true,
+      lax_html_blocks: true,
+      strikethrough: true
+    }
+    markdown = Redcarpet::Markdown.new(html_render, options)
+    markdown.render(text)
+  end
 end
